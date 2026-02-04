93c8f50ed15d3d32579ecff4b32a7de6cf821f1d
beata-teck <batatefera7@gmail.com>
2026-02-04 20:52:17 +0300

    day2

---
+++ spec-first-app/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import {ref} from 'vue';
+const name = ref('');      
+</script> 
+
+<template>
+  <div>
+    
+    <h2>Enter Your Name</h2>
+    <input v-model="name" placeholder="write Ur name"/>
+    <h2 v-if="name !== ''">Hello {{ name }}!</h2>
+
+
+    
+  </div>
+</template>
+
+
+<style scoped>
+  h2 {
+    color: #42b983;
+  }
+</style>
+
--- spect-first-app/src/App.vue
+++
@@ -1,3 +0,0 @@
-<template>
-  <h1>Hello SPEC Developers</h1>
-</template>
